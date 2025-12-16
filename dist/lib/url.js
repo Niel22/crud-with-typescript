f8ee62ba9a725ca74b831893fed59bcd8771a027
Niel22 <niel2264@gmail.com>
2025-12-16 09:04:17 +0100

    Stop tracking dist and logs

--- dist/lib/url.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.url = url;
-function url(resourcePath) {
-    return `${process.env.APP_URL}/${resourcePath}`;
-}
